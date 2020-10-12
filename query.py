73b4fc4bdddb1c66c0bc01b66ca5ec0636b9b291
ThePythonist <gbt505@hotmail.com>
2020-10-12 01:10:44 +0100

    first commit

---
+++ query.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.7
+
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="movies"
+)
+
+cursor = db.cursor()
+
+with open("mysql.sql") as file:
+    queries = list(x.strip()+";" for x in file.read().split(";\n"))
+
+qCount = len(queries)
+
+for i, query in enumerate(queries):
+    print(f"[{i}/{qCount}] - {query}")
+    cursor.execute(query)
