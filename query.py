58511fccb620106f0b590e5ed303b9b05668d25e
ThePythonist <gbt505@hotmail.com>
2020-10-12 01:30:39 +0100

    updated

--- query.py
+++ query.py
@@ -4,12 +4,13 @@ import mysql.connector
 
 db = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="",
-    database="movies"
+    user="user",
+    password="correcthorsebatterystaple",
 )
 
 cursor = db.cursor()
+
+cursor.execute("CREATE DATABASE movies CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;")
 
 with open("mysql.sql") as file:
     queries = list(x.strip()+";" for x in file.read().split(";\n"))
